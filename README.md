89bcf11fd2b92f37ac642daa81e2a91bafc5afc3
Gentian Polovina <gentip@gmail.com>
2020-05-07 17:21:12 +0200

    pages generator + readme update

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# yo-nuxt
-Nuxt Yeoman Generator
+# Nuxt Yeoman Generator
+This is a NuxtJS Yeoman Generator. It helps to quickly scaffold some of the NuxtJS elements.
+
+Available generators:
+- pages
+
+# Requirements
+- [Yeoman](https://yeoman.io/)
+
+# Getting started
+## Install Yeoman
+```
+npm install -g yo
+```
+## Link generator-nuxt npm module
+Since this generator is not yet available as a global npm module you have to symlink it as a local one.
+From the root of the generatr folder type:
+```
+npm link
+```
+or if you're using Yarn
+```
+yarn link
+```
+and in your project folder type:
+```
+yarn link "generator-nuxt"
+```
+# Use the generator
+## Pages Generator
+This commands help you scaffold Nuxt pages
+```bash
+yo nuxt:pages
+```
+
+```
+```
+
+```
+```
--- generators/pages/index.js
+++ generators/pages/index.js
@@ -1,7 +1,47 @@
 const Generator = require("yeoman-generator")
 
 module.exports = class extends Generator {
-    prompting() { }
-    writing() { }
-    paths() { }
+  async prompting() {
+    this.prompts = await this.prompt([
+      {
+        type: "input",
+        name: "pages",
+        message: "Insert page path relative to the project including children pages"
+      },
+      {
+        type: "input",
+        name: "path",
+        message: "Target path where the pages will be written"
+      }
+    ])
+  }
+  writing() {
+    const rgxFileExt = /^(.*\.(?!a-z$))[^.]*$/ig
+    const segments = JSON.stringify(this.prompts.pages)
+      .replace(/"|'/g, '')
+      .split(/\/|\\/g)
+      .filter(s => s !== "")
+    if (segments.length > 1) {
+      let _path = ""
+      for (let _index = 0; _index < segments.length; _index++) {
+        _path += `/${segments[_index]}`
+        let _pagepath = _index === 0 ? "" : _path
+        this._createPageWithFolder(segments[_index], _pagepath)
+      }
+    } else {
+      const file = segments[0].match(rgxFileExt)
+      if (file) {
+        const filename = segments[0].split('.')[0]
+        this.fs.copyTpl(this.templatePath("page.vue"), this.destinationPath(`pages/${this.prompts.path}/${segments[0]}`), { ComponentName: filename })
+      } else {
+        this._createPageWithFolder(segments[0])
+      }
+    }
+  }
+  _createPageWithFolder(pagename, path = "") {
+    const _isFirst = path === ""
+    const _pagePath = _isFirst ? pagename : `${path}`
+    this.fs.copyTpl(this.templatePath("parent.vue"), this.destinationPath(`pages/${this.prompts.path}/${_pagePath}.vue`))
+    this.fs.copyTpl(this.templatePath("children.vue"), this.destinationPath(`pages/${this.prompts.path}/${_pagePath}/index.vue`), { ComponentName: pagename })
+  }
 }
